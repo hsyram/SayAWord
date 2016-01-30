d1c70b2349bfc9db8fdb0924b4f9e90acb4e5ccb
Mary <mr.sheikhha@gmail.com>
2016-01-30 20:19:55 +0200

    add splashActivity and change wizard images

--- app/src/main/java/com/barantech/sayaword/SplashActivity.java
+++ app/src/main/java/com/barantech/sayaword/SplashActivity.java
@@ -1,172 +1,70 @@
 package com.barantech.sayaword;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
+import android.animation.Animator;
+import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
 import android.view.View;
-import android.view.ViewGroup;
 
 public class SplashActivity extends AppCompatActivity {
-    /**
-     * The number of pages (wizard steps) to show in this demo.
-     */
-    private static final int NUM_PAGES = 3;
 
-    /**
-     * The pager widget, which handles animation and allows swiping horizontally to access previous
-     * and next wizard steps.
-     */
-    private ViewPager mPager;
-
-    /**
-     * The pager adapter, which provides the pages to the view pager widget.
-     */
-    private PagerAdapter mPagerAdapter;
-    private View vIndicator0;
-    private View vIndicator1;
-    private View vIndicator2;
-    private View mPreActive;
-    private static final float MIN_SCALE = 0.75f;
+    View mIvLogo;
+    private int mHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        mIvLogo = findViewById(R.id.iv_logo);
+        mIvLogo.animate()
+                .alpha(1f)
+                .setDuration(1000);
 
-        // Instantiate a ViewPager and a PagerAdapter.
-        mPager = (ViewPager) findViewById(R.id.pager);
-        vIndicator0 = findViewById(R.id.v_indicator_0_shadow);
-        vIndicator1 = findViewById(R.id.v_indicator_1_shadow);
-        vIndicator2 = findViewById(R.id.v_indicator_2_shadow);
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        mHeight = size.y;
 
-        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
-        mPager.setAdapter(mPagerAdapter);
-        mPager.setPageTransformer(true, new SplashPageTransformer());
-
-        mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                View view = getIndicator(position);
-                if (view != null)
-                    if (positionOffset <= 0) { // [-1,0]
-                        mPreActive = view;
-                        view.setAlpha(1);
-                        view.setScaleX(1);
-                        view.setScaleY(1);
-
-                    } else if (positionOffset <= 1) { // (0,1]
-                        if(positionOffset >= 0.5)
-                            mPreActive.setAlpha(0);
-                        // Fade the page out.
-                        view.setAlpha(1 - positionOffset);
-
-                        // Scale the page down (between MIN_SCALE and 1)
-                        float scaleFactor = MIN_SCALE
-                                + (1 - MIN_SCALE) * (1 - Math.abs(positionOffset));
-                        view.setScaleX(scaleFactor);
-                        view.setScaleY(scaleFactor);
-
+            public void run() {
+                mIvLogo.animate()
+                        .translationY(-mHeight)
+                        .setDuration(1000);
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(new Intent(SplashActivity.this, WizardActivity.class));
+                        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
                     }
+                },700);
+//                .setListener(new Animator.AnimatorListener() {
+//                    @Override
+//                    public void onAnimationStart(Animator animation) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onAnimationEnd(Animator animation) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onAnimationCancel(Animator animation) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onAnimationRepeat(Animator animation) {
+//
+//                    }
+//                });
             }
+        }, 3*1000);
 
-            @Override
-            public void onPageSelected(int position) {
-
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-            }
-        });
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (mPager.getCurrentItem() == 0) {
-            // If the user is currently looking at the first step, allow the system to handle the
-            // Back button. This calls finish() on this activity and pops the back stack.
-            super.onBackPressed();
-        } else {
-            // Otherwise, select the previous step.
-            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
-        }
-    }
-
-    private View getIndicator(int position) {
-        switch (position) {
-            case 0:
-                return vIndicator0;
-            case 1:
-                return vIndicator1;
-            case 2:
-                return vIndicator2;
-            default:
-                return null;
-        }
-
-    }
-
-    /**
-     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
-     * sequence.
-     */
-    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
-        public ScreenSlidePagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            switch (position){
-                case 0:
-                    return new WizardOneFragment();
-                case 1:
-                    return new WizardTwoFragment();
-                case 2:
-                    return new WizardThreeFragment();
-            }
-            return new WizardOneFragment();
-        }
-
-        @Override
-        public int getCount() {
-            return NUM_PAGES;
-        }
-    }
-
-    public static class WizardOneFragment extends Fragment {
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            return inflater.inflate(
-                    R.layout.wizard_one, container, false);
-
-        }
-    }
-    public static class WizardTwoFragment extends Fragment {
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            return inflater.inflate(
-                    R.layout.wizard_two, container, false);
-
-        }
-    }
-    public static class WizardThreeFragment extends Fragment {
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            return inflater.inflate(
-                    R.layout.wizard_three, container, false);
-
-        }
     }
 }
--- app/src/main/java/com/barantech/sayaword/WizardPageTransformer.java
+++ app/src/main/java/com/barantech/sayaword/WizardPageTransformer.java
@@ -8,14 +8,20 @@ import android.view.View;
  * <br/>
  * Created by mary on 12/22/15.
  */
-public class SplashPageTransformer implements ViewPager.PageTransformer {
+public class WizardPageTransformer implements ViewPager.PageTransformer {
     View v1;
     View v2;
+    View tv1;
+    View tv2;
+    View tv3;
 
     public void transformPage(View view, float position) {
         int pageWidth = view.getWidth();
         v1 = view.findViewById(R.id.v1);
         v2 = view.findViewById(R.id.v2);
+        tv1 = view.findViewById(R.id.tv1);
+        tv2 = view.findViewById(R.id.tv2);
+        tv3 = view.findViewById(R.id.tv3);
 
         if (position < -1) { // [-Infinity,-1)
             // This page is way off-screen to the left.
@@ -29,6 +35,9 @@ public class SplashPageTransformer implements ViewPager.PageTransformer {
 //            view.setScaleY(1);
             v1.setTranslationX((position) * (pageWidth / 4));
             v2.setTranslationX((position) * (pageWidth / 2));
+            tv1.setTranslationX((position) * (pageWidth));
+            tv2.setTranslationX((position) * (pageWidth));
+            tv3.setTranslationX((position) * (pageWidth));
 
         } else if (position <= 1) { // (0,1]
 //            view.setAlpha(1 - position);
@@ -40,6 +49,9 @@ public class SplashPageTransformer implements ViewPager.PageTransformer {
 
             v1.setTranslationX((position) * (pageWidth / 4));
             v2.setTranslationX((position) * (pageWidth / 2));
+            tv1.setTranslationX((position) * (pageWidth));
+            tv2.setTranslationX((position) * (pageWidth));
+            tv3.setTranslationX((position) * (pageWidth));
 
         } else { // (1,+Infinity]
             // This page is way off-screen to the right.
